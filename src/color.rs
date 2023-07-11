75efe0449c4d7401232f0f73b81975cd3f31ef44
deotime <89555032+DeoTimeTheGithubUser@users.noreply.github.com>
2023-07-10 23:27:12 -0500

    updates

--- src/color.rs
+++ src/color.rs
@@ -1,4 +1,6 @@
-#[derive(Default, Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
 pub struct Color {
     hex: u32,
 }
@@ -6,5 +8,31 @@ pub struct Color {
 impl Color {
     pub const fn from_hex(hex: u32) -> Color {
         Color { hex }
+    }
+
+    pub const fn from_rgb(r: u8, g: u8, b: u8) -> Color {
+        Self::from_hex(((r as u32) << 16) + ((g as u32) << 8) + (b as u32))
+    }
+
+    pub const fn r(&self) -> u8 {
+        ((self.hex << 16) & 0xFF) as u8
+    }
+
+    pub const fn g(&self) -> u8 {
+        ((self.hex << 8) & 0xFF) as u8
+    }
+
+    pub const fn b(&self) -> u8 {
+        (self.hex & 0xFF) as u8
+    }
+
+    pub const fn rgb(&self) -> (u8, u8, u8) {
+        (self.r(), self.g(), self.b())
+    }
+}
+
+impl Default for Color {
+    fn default() -> Self {
+        Color::from_hex(0xFFFFFFFF)
     }
 }
